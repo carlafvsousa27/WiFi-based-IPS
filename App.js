0b132e954b1658c8af8529e93bd214722730a35e
Carla Sousa <carlafvsousa27@gmail.com>
2022-10-18 17:03:33 +0100

    Página Inicial

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function App() {
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: '#C4DDFF',
+    height: 896,
+  },
+  topContainer: {
+    paddingTop: 200,
+    paddingBottom: 50,
+    backgroundColor: '#C4DDFF',
+    alignItems: 'center'
+  },
+  mediumContainer: {
+    paddingTop: 100,
+    backgroundColor: '#C4DDFF',
+    alignItems: 'center'
+  },
+  bottomContainer: {
+    paddingTop: 50,
+    backgroundColor: '#C4DDFF',
+    alignItems: 'center'
+  },
+  logo: {
+    width: 100,
+    height: 100
+  },
+  baseText: {
+    fontSize: 26,
+    color: '#001D6E'
+  },
+  titleText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: '#001D6E'
+  }
+});
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.page}>
+      <View style={styles.topContainer}>
+        <Text style={styles.titleText}>
+          Welcome
+        </Text>
+      </View>
+      <View style={styles.mediumContainer}>
+        <Image 
+          style={styles.logo}
+          source={require('./assets/logo.png')}>
+        </Image>
+      </View>
+      <View style={styles.topContainer}>
+        <Text style={styles.baseText}>
+          Let's Go!
+        </Text>
+      </View>
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
